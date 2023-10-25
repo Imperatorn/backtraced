adbb36558868d53f0e6dc3528485d116b6b078db
johan.forsberg <johan.forsberg@akerstroms.se>
2023-10-25 23:37:15 +0200

    execinfo doesn't exist in Posix

--- source/seh.d
+++ source/seh.d
@@ -127,7 +127,7 @@ version (OSX)
     import core.stdc.stdio : fprintf, stderr, sprintf, fgets, fclose, FILE;
     import core.sys.posix.stdio;
     import core.sys.posix.signal;
-    import core.sys.posix.execinfo;
+    import core.sys.linux.execinfo;
     import core.sys.posix.dlfcn;
     import core.sys.linux.link;
     import core.demangle : demangle;
